3dc9d1674eb8769760df6cdc3ca44b54d87d8ef4
dcferg13 <danielleferg13@gmail.com>
2022-09-26 15:22:59 -0700

    updating MechaCarChallenge.R file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,9 +1,20 @@
+#Deliverable 1
 #Step 3 Use library() function to load dplyr package
 library(dplyr)
-#Step 4 Import and Read the CSV file.
-MechaCar <- read.csv(file="MechaCar_mpg.csv")
+#Step 4 Import and Read the MechaCar_mpg.csv file.
+MechaCar <- read.csv(file="Resources/MechaCar_mpg.csv")
 #Step 5 Perform Linear Regression
 reg <- lm(vehicle_length~vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg, data = MechaCar)
 reg
 #Step 6 Using Summary() function, determine the p-value and r-squared value for lm
 summary(reg)
+
+#Deliverable 2
+#Step 2 Import and Read the Suspension_Coil.csv file.
+Suspension <- read.csv(file="Resources/Suspension_Coil.csv")
+#Step 3 Create a total_summary dataframe using summarize()
+total_summary <- summarise(Suspension, Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+#Step 4 Create a lot_summary dataframe using group_by()and summarize()
+lot <- group_by(Suspension, Manufacturing_Lot)
+lot_summary <- summarize(lot, Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
